6f7df1b09cf6e1f90ad73336577422be7284de16
Okunoda <okunoda@outlook.com>
2023-02-23 22:52:13 +0800

    剑指offer-day1

---
+++ offer/src/CQueue.java
@@ -0,0 +1,37 @@
+/**
+ * @author Okunoda
+ * @date 2023年02月23日22:29
+ */
+
+class CQueue {
+
+    int tail;
+    int head;
+    int[] nums;
+    public CQueue() {
+        nums = new int[10000];
+        tail = 0;
+        head = 0;
+    }
+
+    public void appendTail(int value) {
+        if(isFull())return;
+        nums[tail] = value;
+        tail = (tail+1)%10000;
+
+    }
+
+    public int deleteHead() {
+        if(tail==head) return -1;
+        int res = nums[head];
+        head = (head+1)%10000;
+        return res;
+
+    }
+
+    private boolean isFull(){
+        return tail + 1 == head;
+    }
+}
+
+
---
+++ offer/src/MinStack.java
@@ -0,0 +1,46 @@
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * @author Okunoda
+ * @date 2023年02月23日22:42
+ */
+
+class MinStack {
+
+    /** initialize your data structure here. */
+    int min  = Integer.MAX_VALUE;
+    Stack<Integer> stack ;
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(x < min){
+            min = x;
+        }
+    }
+
+    public void pop() {
+        int temp = stack.pop();
+        if(temp == min){
+            min = Integer.MAX_VALUE;
+            Iterator<Integer> iterator = stack.iterator();
+            while(iterator.hasNext()){
+                int item = iterator.next();
+                if(item<min){
+                    min = item;
+                }
+            }
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return min;
+    }
+}
