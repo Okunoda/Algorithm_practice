eda5d76258bf58b5abde8aa5cac7549fcd0bc64e
okunoda <okunoda@outlook.com>
2024-09-14 09:35:59 +0800

    medium——排序链表（归并解法）

---
+++ leetcode/src/main/java/okunoda/Medium_sortList.java
@@ -0,0 +1,43 @@
+package okunoda;
+
+/**
+ * @author Erywim 2024/9/13
+ */
+public class Medium_sortList {
+    /**
+     * 归并排序的思想，牛的
+     作者：Krahets
+     链接：https://leetcode.cn/problems/sort-list/solutions/13728/sort-list-gui-bing-pai-xu-lian-biao-by-jyd/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param head
+     * @return
+     */
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+}
