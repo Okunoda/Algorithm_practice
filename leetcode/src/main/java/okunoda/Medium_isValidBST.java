da257161a3922c230f63d9268df368b86624d174
okunoda <okunoda@outlook.com>
2024-09-21 12:13:38 +0800

    Medium———验证二叉搜索树

---
+++ leetcode/src/main/java/okunoda/Medium_isValidBST.java
@@ -0,0 +1,33 @@
+package okunoda;
+
+/**
+ * @author Erywim 2024/9/19
+ */
+public class Medium_isValidBST {
+    /**
+     *
+     作者：灵茶山艾府
+     链接：https://leetcode.cn/problems/validate-binary-search-tree/solutions/2020306/qian-xu-zhong-xu-hou-xu-san-chong-fang-f-yxvh/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+     s
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode node, long left, long right) {
+        if (node == null) {
+            return true;
+        }
+        long x = node.val;
+        return left < x && x < right &&
+                isValidBST(node.left, left, x) &&
+                isValidBST(node.right, x, right);
+    }
+
+
+}
---
+++ leetcode/src/main/java/okunoda/TreeNode.java
@@ -0,0 +1,24 @@
+package okunoda;
+
+/**
+ * @author Erywim 2024/9/19
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
